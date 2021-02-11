96a25d8e43cf9e8c4e35f00b18ceee4ff7651d88
ayumitk <ayumee528@gmail.com>
2021-02-11 14:53:03 -0800

    added loading pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
   purge: [],
-  darkMode: 'media', // or false or 'class'
+  darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
       transparent: 'transparent',
